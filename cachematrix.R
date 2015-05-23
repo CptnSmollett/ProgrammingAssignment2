70b59469054ac3942058b8ef2e8e5214ebfa4643
CptnSmollett <andrew.sverdlov@gmail.com>
2015-05-24 01:58:54 +0300

    R programming - Assigment 2 commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,86 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a code file for programming assignment 2 in R Programming course
+## by Dr. Peng.
+## These functions inverse a matrix with caching as requested in original asssignment.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix(x)
+## This function returns a list of basic operations for cached matrix inversion.
+## It takes a matrix as a single argument. 
+## Arguments:
+##      x - matrix for inversion, default value is empty matrix
+## Output list elements:
+##      set - function taking a matrix as a single possible argument.
+##            Other types are not coerced to matrix class.
+##            A check for matrix class and square matrix is performed.
+##            Both tests return warning message in case of failure.
+##            This function drops cached inversed matrix value when used.
+##            Used to cache a matrix value for further inversion.
+##      get - funtion with no arguments returns matrix value from cache.
+##      setInversed - function taking a matrix as a single possible argument.
+##                    A check for matrix class and square matrix is performed.
+##                    Both tests return warning message in case of failure.
+##                    Used to cache an inversed matrix value.
+##      getInversed - funtion with no arguments returns inversed matrix value
+##                    from cache.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inversed <- NULL
+    
+    set <- function(y) {
+        if (!is.matrix(y)) {
+            warning("Argument passed is not a matrix",
+                    immediate. = TRUE)
+        }
+        else if (ncol(y) != nrow(y)) {
+            warning("Argument passed is not a square matrix",
+                    immediate. = TRUE)
+        }
+        
+        x <<- y
+        inversed <<- NULL
+    }
+    
+    get <- function() x
+    
+    setInversed <- function(i) {
+        if (!is.matrix(i)) {
+            warning("Argument passed is not a matrix",
+                    immediate. = TRUE)
+        }
+        else if (ncol(i) != nrow(i)) {
+            warning("Argument passed is not a square matrix",
+                    immediate. = TRUE)
+        }
+        
+        inversed <<- i
+    }
+    
+    getInversed <- function() inversed
+    
+    list(get = get, set = set, setInversed = setInversed, getInversed = getInversed)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x, ...)
+## Function taking a matrix value to inverse it using cache.
+## If cache contains any inversed matrix value it is returned. Otherwise it is
+## calculated and cached.
+## Arguments:
+##      x - list returned by makeCacheMatrix() function
+##      ... - arguments passed to solve() function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # trying cache first
+    i <- x$getInversed()
+    if (!is.null(i)) {
+        message("Using cached value")
+        return(i)
+    }
+    
+    # bad luck, no cached value available thus need to compute it in an honest way
+    matrix <- x$get()
+    i <- solve(matrix)
+    x$setInversed(i)
+    i
 }
